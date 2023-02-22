cb95b8c171cb32aa004bc383f634215fdc7ede98
Artem <124471498+fEternityFate@users.noreply.github.com>
2023-02-22 20:18:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function reverse(number){
+   var number = number + '';
+   return number.split('').reverse().join('');
+}
+console.log(reverse(214))
+console.log(reverse(540))
+console.log(reverse(948))
+console.log(reverse(001))
+console.log(reverse(123))
+console.log(reverse(907))
+console.log(reverse(406))
